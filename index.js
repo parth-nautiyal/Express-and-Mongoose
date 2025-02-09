18ced4f25265e86485600ccd2c08d2c7446a76a6
Parth Nautiyal <parthnautiyal1016@gmail.com>
2025-02-09 15:34:39 +0530

    added a farm

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Product = require('./models/product.js')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const AppError = require('./AppError')
+const Farm = require('./models/farms')
 
 app.use(methodOverride('_method'))
 app.use(bodyParser.json())
@@ -22,6 +23,23 @@ async function main() {
 
 const categories = ['fruit', 'vegetable', 'dairy']
 
+app.get('/farms', async(req, res) => {
+    const farms = await Farm.find({})
+    res.render('farms/home', { farms })
+})
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+app.get('/farms/:id', async(req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products')
+    res.render('farms/show', { farm })
+})
+app.post('/farms', async(req, res) => {
+    const farm = new Farm(req.body)
+    await farm.save()
+    res.redirect('/farms')
+})
+
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
@@ -34,11 +52,22 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${product._id}`)
 })
 
-app.get('/products/new', (req, res) => {
-    res.render('products/new', { categories })
+app.get('/farms/:id/products/new', (req, res) => {
+    res.render('products/new', { categories, id: req.params.id })
 })
-app
-    .get('/products', async (req, res) => {
+app.post('/farms/:id/products', async(req, res) => {
+    const { id } = req.params
+    const farm = await Farm.findById(id)
+    const { name, price, category } = req.body
+    const product = new Product({ name, price, category })
+    farm.products.push(product)
+    product.farm = farm
+    await farm.save()
+    await product.save()
+    res.redirect(`/farms/${id}`)
+})
+
+app.get('/products', async (req, res) => {
         const { category } = req.query;
         if (category) {
             const products = await Product.find({ category })
--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const productSchema = new mongoose.Schema({
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
     name:{
         type:String,
         required: true
@@ -14,7 +16,13 @@ const productSchema = new mongoose.Schema({
         type: String,
         lowercase: true,
         enum: ['fruit','vegetable','dairy']
-    }
+    },
+    farms :[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Farm'
+        }
+    ]
 })
 
 const Product = mongoose.model('Product',productSchema)
